243138bd149ec056a7248d3078aab59aad1b88b3
Matt Larson <larsonmattr@gmail.com>
2020-08-19 16:11:26 -0500

    Updated JSON and classes derived from JSON.

---
+++ src/CryoProject/Records.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryoProject
+{
+
+    public class Rootobject
+    {
+        public Page[] pages { get; set; }
+    }
+
+    public class Page
+    {
+        public string title { get; set; }
+        public Field[] fields { get; set; }
+    }
+
+    public class Field
+    {
+        public string name { get; set; }
+        public string type { get; set; }
+        public string hint { get; set; }
+        public Option[] options { get; set; }
+        public string unit { get; set; }
+    }
+
+    public class Option
+    {
+        public string name { get; set; }
+    }
+
+}
