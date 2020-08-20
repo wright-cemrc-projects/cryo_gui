d30aaceb79a0f89021afa6577f766983e344413e
Matt Larson <larsonmattr@gmail.com>
2020-08-20 09:31:04 -0500

    Fix namespace clash.

--- src/CryoProject/Records.cs
+++ src/CryoProject/Records.cs
@@ -6,30 +6,31 @@ using System.Threading.Tasks;
 
 namespace CryoProject
 {
+    namespace Config { 
 
-    public class Rootobject
-    {
-        public Page[] pages { get; set; }
+        public class Rootobject
+        {
+            public Page[] pages { get; set; }
+        }
+
+        public class Page
+        {
+            public string title { get; set; }
+            public Field[] fields { get; set; }
+        }
+
+        public class Field
+        {
+            public string name { get; set; }
+            public string type { get; set; }
+            public string hint { get; set; }
+            public Option[] options { get; set; }
+            public string unit { get; set; }
+        }
+
+        public class Option
+        {
+            public string name { get; set; }
+        }
     }
-
-    public class Page
-    {
-        public string title { get; set; }
-        public Field[] fields { get; set; }
-    }
-
-    public class Field
-    {
-        public string name { get; set; }
-        public string type { get; set; }
-        public string hint { get; set; }
-        public Option[] options { get; set; }
-        public string unit { get; set; }
-    }
-
-    public class Option
-    {
-        public string name { get; set; }
-    }
-
 }
