f6e5f44d0f5cef8f3972041ecc71621ea9011806
Matt Larson <larsonmattr@gmail.com>
2020-09-01 11:59:19 -0500

    CryoGUI can now read a defaults.json to provide standard values.

--- src/CryoProject/MainWindow.xaml.cs
+++ src/CryoProject/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,8 +26,22 @@ namespace CryoProject
         {
             InitializeComponent();
 
-            // Create the application state and set for the first time.
             Metadata state = new Metadata();
+
+            string filename = "defaults.json";
+
+            // Try loading "default.json" which could provide per instrument settings.
+            if (File.Exists(filename))
+            {
+                using (StreamReader file = File.OpenText(filename))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    state = (Metadata)serializer.Deserialize(file, typeof(Metadata));
+                    state.Date = DateTime.Now;
+                }
+            }
+
+            // Create the application state and set for the first time.
             Content = new PageProjectInfo(state);
         }
     }
